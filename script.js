09882b75191ce49c560d0a82c81163b6cdc14f14
dorrsakh <dorrsakh@gmail.com>
2024-01-03 18:00:14 +0330

    program

---
+++ script.js
@@ -0,0 +1,11 @@
+let arr = prompt("Enter a list of numbers of your choice: (for example: 1 2 -44 68 39)").split(" ");
+let res = [];
+
+for (let num of arr) {
+    num = Number(num);
+    if (num % 2 === 0){
+        res.push(num);
+    }
+}
+
+console.log(res.length);
